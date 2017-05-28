59f4a1a73d55692e6d353440d43352343e8a0acf
Kostadin Golev <kotseto@gmail.com>
2017-05-28 16:15:56 +0300

    @Nested and @DisplayName example in the making (2/N): @DisplayName is added

--- junit5-sample/src/test/java/sample/nested/NestedTest.java
+++ junit5-sample/src/test/java/sample/nested/NestedTest.java
@@ -1,12 +1,14 @@
 package sample.nested;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import sample.Library;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+@DisplayName("Book is added to library")
 class BookAddedTest {
 
     Books books;
@@ -18,15 +20,19 @@ class BookAddedTest {
         library = new Library(books);
     }
 
+    @DisplayName("When book exists -> amount++")
     @Test
     void whenBookExistsThenIncrementAmount() {}
 
+    @DisplayName("When book exists -> check amount > limit")
     @Test
     void whenBookExistsThenCheckAmountMoreThenN() {}
 
+    @DisplayName("When book doesn't exist -> create it")
     @Test
     void whenBookDoesNotExistsThenCreateABook() {}
 
+    @DisplayName("When book doesn't exist -> send new book notification")
     @Test
     void whenBookDoesNotExistsThenSendNewBookNotification() {}
 }
